59d1e12f84c53220fa9808dcf5a6f6936cf4b292
Diogo Pereira <sir.suriv@gmail.com>
2016-11-21 20:28:18 +0000

    Add alpha2to3.py
    Script to convert ISO alpha2 to alpha3 country codes.

---
+++ alpha2to3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+
+import pandas as pd
+import pycountry
+
+
+def main():
+    os.chdir("data/out")
+
+    for table in ["country_tag", "country_tag_month", "country_tag_year"]:
+        df = pd.read_csv(table + ".csv", header=None)
+        df[0] = df[0].map(lambda x: pycountry.countries.get(alpha2=x).alpha3)
+        df.to_csv(table + ".csv", header=False, index=False)
+
+
+if __name__ == "__main__":
+    main()
