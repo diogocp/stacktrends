19731847e2a065244ce39a05b3f4b368860b0bb7
Diogo Pereira <sir.suriv@gmail.com>
2016-11-27 01:41:15 +0000

    New index.html with a layout for the 4 charts.

--- html/js/barchart.js
+++ html/js/barchart.js
@@ -2,8 +2,9 @@ import d3 from "d3";
 import d3Promise from "d3.promise";
 
 export default class {
-    constructor() {
+    constructor(parentId) {
         this.dataset = this.loadData("data/country_tag.csv");
+        this.container = d3.select("#" + parentId);
         window.addEventListener(
                 "tagSelectionChange",
                 this.onTagSelectionChange.bind(this),
@@ -11,30 +12,33 @@ export default class {
     }
 
     async draw(tags) {
-        d3.selectAll("svg").remove();
-
         var data = (await this.dataset)["USA"];
         data = data.filter(item => {
             return tags.indexOf(item.tag) != -1;
         });
 
-        var svg = d3.select("div#barchart")
+        var containerRect = this.container.node().getBoundingClientRect();
+
+        this.container.select("svg").remove();
+        var svg = this.container
             .append("svg")
-            .attr("width", "800")
-            .attr("height","500");
-        var margin = {top: 20, right: 20, bottom: 30, left: 100};
+            .attr("width", containerRect.width - 10)
+            .attr("height", containerRect.height - 10);
+        var margin = {top: 20, right: 20, bottom: 30, left: 80};
         var width = +svg.attr("width") - margin.left - margin.right;
         var height = +svg.attr("height") - margin.top - margin.bottom;
-
-        var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-        var y = d3.scale.linear().range([height, 0]);
 
         var g = svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-        x.domain(data.map(function(d) { return d.tag; }));
-        y.domain([0, d3.max(data, function(d) { return d.count; })]);
+        var x = d3.scale
+            .ordinal()
+            .rangeRoundBands([0, width], .1)
+            .domain(data.map(d => d.tag));
+        var y = d3.scale
+            .linear()
+            .range([height, 0])
+            .domain([0, d3.max(data, d => d.count)]);
 
         g.append("g")
             .attr("class", "axis axis--x")
@@ -55,11 +59,10 @@ export default class {
             .data(data)
             .enter().append("rect")
             .attr("class", "bar")
-            .attr("x", function(d) { return x(d.tag); })
-            .attr("y", function(d) { return y(d.count); })
+            .attr("x", d => x(d.tag))
+            .attr("y", d => y(d.count))
             .attr("width", x.rangeBand())
-            .attr("height", function(d) { return height - y(d.count);
-            });
+            .attr("height", d => height - y(d.count));
 
         this.chart = g;
     }
