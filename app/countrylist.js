19d17da601405f601866f24d15cafdbc08daf238
Diogo Pereira <sir.suriv@gmail.com>
2016-12-02 00:10:15 +0000

    Drop dependency on country-data
    Using our own list of countries saves about 100 kB.

--- app/countrylist.js
+++ app/countrylist.js
@@ -1,17 +1,23 @@
 import $ from "jquery";
 import chosen from "chosen-js";
-import {countries} from "country-data";
+import d3Promise from "d3.promise";
+
 
 export default class {
     constructor(container) {
-        var listHtml = '<option value=""></option>';
-        countries.all.forEach(item => {
-            listHtml += `<option value="${item.alpha3}">${item.name}</option>`;
-        });
+        this.countries = this.loadData("data/countries.json");
 
-        this.list = $("#" + container);
-        this.list.append(listHtml);
-        this.list.chosen();
+        this.countries.then(data => {
+            var listHtml = '<option value=""></option>';
+
+            for(var c in data) {
+                listHtml += `<option value="${c}">${data[c].name}</option>`;
+            }
+
+            this.list = $("#" + container);
+            this.list.append(listHtml);
+            this.list.chosen();
+        });
 
         window.addEventListener(
                 "countryClick",
@@ -22,5 +28,9 @@ export default class {
     onCountryClick(event) {
         this.list.val(event.detail);
         this.list.trigger("chosen:updated");
+    }
+
+    loadData(filename) {
+        return d3Promise.json(filename);
     }
 }
